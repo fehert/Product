7486300f988a64aa495f9291c8ee42c8d8b20da6
ftomi <fehert@kkszki.hu>
2024-10-01 14:18:46 +0200

    harmadik commit

---
+++ ProductApi/Connect.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApi
+{
+    public class Connect
+    {
+        public MySqlConnection Connection { get; set; }
+        private string Host;
+        private string Database;
+        private string Username;
+        private string Password;
+        public string ConnectionString { get; set; }
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "auto";
+            Username = "root";
+            Password = "";
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + Username + ";PASSWORD=" + Password + ";SslMode=None";
+            Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
---
+++ ProductApi/Models/product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductApi.Models
+{
+    public class product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Price {  get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
