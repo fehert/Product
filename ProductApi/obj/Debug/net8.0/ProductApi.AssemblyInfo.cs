b282cf0f4e5ca1e6f2e52cdf0017d4dd343a885e
ftomi <fehert@kkszki.hu>
2024-10-01 13:55:15 +0200

    masodik commit

--- ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+973b01761ec522b677b20a2485870b81bf1bfc46")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
